2275d930b8689753e20d00cbee874a8236f66621
GarifullinII <ildaridrisovich@gmail.com>
2024-10-18 01:03:47 +0400

    add TableView

---
+++ FlashChat/Extensions/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  FlashChat
+//
+//  Created by Ildar Garifullin on 17.10.2024.
+//
+
+struct K {
+    
+}
---
+++ FlashChat/Models/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by Ildar Garifullin on 17.10.2024.
+//
+
+import Foundation
